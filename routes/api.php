39444e29d8896984224cbe1bdb071f758d544345
Namrata Gursale <namrata@Namratas-MacBook-Air.local>
2017-08-04 14:50:26 +0530

    added api for mobile

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/user/posts', 'Register\RegistrationController@store');
+	
+Route::get('/users/{user}', 'Register\RegistrationController@show');
